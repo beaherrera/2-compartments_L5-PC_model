a9c37252adf81c6209a3857aa6a9f3377f8aecfa
Beatriz Herrera <bea.herreraf@gmail.com>
2022-03-24 12:32:20 -0400

    update
    updated code for saving simulated data

--- L5PCs/lfp_calculation.m
+++ L5PCs/lfp_calculation.m
@@ -30,22 +30,21 @@ for trial=1:ntrials
     
     %% loading the simulated data
     
-    cd ../
-    file = ['GenerateFigures\FiguresData\' fileL5PCS];
+    file = fullfile('..', 'GenerateFigures', 'FiguresData', fileL5PCS);
     
     if Ih
-        load([file '\currentsTrial' num2str(trial) '.mat'], 'Ysd');
+        load(fullfile(file, ['currentsTrial' num2str(trial) '.mat']), 'Ysd');
     else
-        load([file '\currentsNoIhTrial' num2str(trial) '.mat'], 'Ysd');
+        load(fullfile(file, ['currentsNoIhTrial' num2str(trial) '.mat']), 'Ysd');
     end
     
-    cd L5PCs
     
     %% compartments location
     
     Ids = Ysd; % transmembrane currents of each point-source per neuron
     ls = 890; % [um]length of the apical dendrites
-    sources_number = length(Ids(1,:)); % number of sources in the column (all neurons)
+    sources_number = length(Ids(1,:)); % number of sources in the column 
+    % (all neurons)
     n = 5; % number of point sources per neuron
     unit_number = sources_number/n; % number of neurons
     rng default; % For reproducibility
@@ -55,14 +54,21 @@ for trial=1:ntrials
     % - z (um)
     a = 1025;
     b = 1450;
-    rNum = rand(1,unit_number); % generating the random numbers for the z-direction
+    rNum = rand(1,unit_number); % generating the random numbers for the
+    % z-direction
     ii = 1;
-    AISZ = (b-a).*rNum + a; % this calculates the z-coordinate of the axon initial segment of each neuron
-    zc(1,ii:unit_number) = (1000-700).*rNum + 700; % this calculates the z-coordinate of the soma
-    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ; ii = ii+1; % initial segment source position
-    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ + 150; ii = ii+1; % basal compartment source position, 150 um below the axon initial segment
-    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ - ls; ii = ii+1; % main-bifurcation point, ls um above the soma
-    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ - ls - 150; % tuft, 150 um above the main-bifurcation point
+    AISZ = (b-a).*rNum + a; % this calculates the z-coordinate of the axon 
+    % initial segment of each neuron
+    zc(1,ii:unit_number) = (1000-700).*rNum + 700; % this calculates the
+    % z-coordinate of the soma
+    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ; ii = ii+1; % initial
+    % segment source position
+    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ + 150; ii = ii+1; % 
+    % basal compartment source position, 150 um below the axon initial segment
+    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ - ls; ii = ii+1; % 
+    % main-bifurcation point, ls um above the soma
+    zc(1,ii*unit_number+1:(ii+1)*unit_number) = AISZ - ls - 150; % tuft, 
+    % 150 um above the main-bifurcation point
     
     % -- calculating the position of each neuron on the plane. The same
     % coordinates are used for all sources per neuron
@@ -91,8 +97,10 @@ for trial=1:ntrials
     Ne = 16; % number of electrodes in the shank
     a = 0.1; % [mm] position of the first electrode
     elec_spacing = 0.1; % [mm] electrode spacing
-    ze = a:elec_spacing:((Ne-1)*elec_spacing + a); % position of the electrodes along z with respect to pia matter
-    el_pos = ze*1e-3;  % [m] electrode positions with respect to the pia surface
+    ze = a:elec_spacing:((Ne-1)*elec_spacing + a); % position of the 
+    % electrodes along z with respect to pia matter
+    el_pos = ze*1e-3;  % [m] electrode positions with respect to the 
+    % pia surface
     cond = 0.323; %[S/m] gray matter conductance
     
     %% Current sources
@@ -100,14 +108,16 @@ for trial=1:ntrials
     
     %% LFP calculation
     if trial==1
-        lfpT = zeros(length(ze), length(Iz(1,:)), ntrials); % preallocating the local field potential matrix
+        lfpT = zeros(length(ze), length(Iz(1,:)), ntrials); % preallocating
+        % the local field potential matrix
     end
     
     lfpt = zeros(length(ze), length(Iz(1,:)));
     % Emapping = zeros(length(ze), sources_number);
     
     for ii=1:Ne
-        [lfpt(ii,:), ~] = lfp_pointSourceMethod(Iz, xc, yc, zc, cond, el_pos(ii), elec_spacing.*1e-3); % mV
+        [lfpt(ii,:), ~] = lfp_pointSourceMethod(Iz, xc, yc, zc, cond, ...
+            el_pos(ii), elec_spacing.*1e-3); % mV
     end
     
     lfpT(:,:,trial) = lfpt;
@@ -131,20 +141,12 @@ title(['Mean LFP across ' num2str(ntrials) ' trials'])
 
 %% save
 
-cd ../
-file = ['GenerateFigures\FiguresData\' fileL5PCS];
-if ~exist(file, 'dir') % checks if the folder already exists
-    mkdir(file);  % creates a folder named 'file'
-end
-
 if Ih % Ih present
-    save([file '\lfp_L5PCs.mat'], 'lfp');
-    save([file '\lfp_L5PCsTrials.mat'], 'lfpT');
+    save(fullfile(file, 'lfp_L5PCs.mat'), 'lfp');
+    save(fullfile(file, 'lfp_L5PCsTrials.mat'), 'lfpT');
 else % Ih blocked
-    save([file '\lfp_L5PCsNoIh.mat'], 'lfp');
-    save([file '\lfp_L5PCsNoIhTrials.mat'], 'lfpT');
+    save(fullfile(file, 'lfp_L5PCsNoIh.mat'), 'lfp');
+    save(fullfile(file, 'lfp_L5PCsNoIhTrials.mat'), 'lfpT');
 end
-
-cd L5PCs
 
 end
